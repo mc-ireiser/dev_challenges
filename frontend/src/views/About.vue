125a73541aff48e2f318dd81e8ddeae51b2eacc4
Jesus Rodriguez <mc.ireiser@gmail.com>
2020-12-07 22:06:09 -0400

    Add about page

---
+++ frontend/src/views/About.vue
@@ -0,0 +1,28 @@
+<template>
+	<section class="hero is-light is-fullheight">
+		<div class="hero-body">
+			<div class="container has-text-centered">
+				<div class="columns">
+					<div class="column is-1"></div>
+					<div class="column">
+						<h1 class="title is-2 mb-5">Jesús Rodríguez</h1>
+						<h2 class="title is-4">Workana Hiring Challenge</h2>
+						<div class="mt-6">
+							<a
+								href="https://github.com/mc-ireiser/dev_challenges"
+								target="_blank"
+								rel="noopener noreferrer"
+								class="button is-warning mr-6 mb-5"
+								>Project repository on github</a
+							>
+							<router-link to="/" class="button is-warning"
+								>Go to back to project</router-link
+							>
+						</div>
+					</div>
+					<div class="column is-1"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+</template>
