427ed193724fa4dc56f015dbe1cdd287a347b339
Jesus Rodriguez <mc.ireiser@gmail.com>
2020-12-07 22:03:46 -0400

    Main application file

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,8 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import router from "./router";
+import App from "./App.vue";
 
-Vue.config.productionTip = false
+import "../node_modules/bulma/css/bulma.css";
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+	router,
+	render: h => h(App)
+}).$mount("#app");
