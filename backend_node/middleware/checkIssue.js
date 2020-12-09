7e0761f46d6ee124f35aa254346e1cfa7ccd9d9d
Jesus Rodriguez <mc.ireiser@gmail.com>
2020-12-09 00:29:02 -0400

    Validation using Joi

--- backend_node/middleware/checkIssue.js
+++ backend_node/middleware/checkIssue.js
@@ -1,14 +1,30 @@
 const redisClient = require("../helpers/redis");
+const Joi = require("joi");
 
-module.exports = async function checkToken(req, res, next) {
+module.exports = async function checkIssue(req, res, next) {
 	const issueNumber = req.params.issue;
-	const issueExist = await redisClient.exists(issueNumber);
+	const issueNumberIsValid = Joi.string().required().validate(issueNumber);
 
-	if (issueExist) {
-		next();
-	} else {
-		res.status(404).json({
-			message: "The requested issue does not exist",
+	if (!issueNumber || !issueNumberIsValid) {
+		res.status(400).json({
+			message: "Issue number is required",
+		});
+		return;
+	}
+
+	try {
+		const issueExist = await redisClient.exists(issueNumber);
+
+		if (issueExist) {
+			next();
+		} else {
+			res.status(404).json({
+				message: "The requested issue does not exist",
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: `Server error ${error}`,
 		});
 	}
 };
--- backend_node/middleware/checkToken.js
+++ backend_node/middleware/checkToken.js
@@ -1,22 +1,31 @@
 const redisClient = require("../helpers/redis");
+const Joi = require("joi");
 
 module.exports = async function checkToken(req, res, next) {
 	const token = req.headers.token;
 
-	if (!token) {
+	const tokenIsValid = Joi.string().required().validate(token);
+
+	if (!token || !tokenIsValid) {
 		res.status(401).json({
 			message: "Auth failed, token is required",
 		});
 		return;
 	}
 
-	const exists = await redisClient.exists(token);
+	try {
+		const exists = await redisClient.exists(token);
 
-	if (exists) {
-		next();
-	} else {
-		res.status(401).json({
-			message: "Unauthorized. A new token is required",
+		if (exists) {
+			next();
+		} else {
+			res.status(401).json({
+				message: "Unauthorized. A new token is required",
+			});
+		}
+	} catch (error) {
+		res.status(500).json({
+			message: `Server error ${error}`,
 		});
 	}
 };
