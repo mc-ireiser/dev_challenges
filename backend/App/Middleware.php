beb72692729b6695868769e2541e6893a1ea34c5
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-02-03 13:49:06 -0400

    Middleware settings

---
+++ backend/App/Middleware.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+use Slim\App;
+
+return function (App $app){
+    $settings = $app->getContainer()->get('settings');
+    $app->addBodyParsingMiddleware();
+    $app->addRoutingMiddleware();
+    $app->addErrorMiddleware($settings['displayErrorDetails'], $settings['logErrors'], $settings['logErrorDetails']);
+};
