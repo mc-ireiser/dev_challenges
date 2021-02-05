22a74621e97bce3bb03dc7e47450dd83b8d53f14
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-02-05 00:28:13 -0400

    Configure database to use

--- backend_node/helpers/redis.js
+++ backend_node/helpers/redis.js
@@ -1,4 +1,11 @@
 const { createNodeRedisClient } = require("handy-redis");
-const client = createNodeRedisClient({ host: "redis", port: 6379 });
+
+const dbNumber = process.env.NODE_ENV === "testing" ? 1 : 0;
+
+const client = createNodeRedisClient({
+	host: "redis",
+	port: 6379,
+	db: dbNumber,
+});
 
 module.exports = client;
