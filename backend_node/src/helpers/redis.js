49cc276b34ff05b74ad27dcc832887b629cf8d74
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-02-06 16:13:44 -0400

    Update redis conection

--- backend_node/src/helpers/redis.js
+++ backend_node/src/helpers/redis.js
@@ -1,7 +1,7 @@
 const { createNodeRedisClient } = require("handy-redis");
 
 const db = process.env.REDISDB;
-const host = process.env.NODE_ENV === "testing" ? "127.0.0.1" : "redis";
+const host = "redis";
 const port = 6379;
 
 const client = createNodeRedisClient({
