9560bb42ca2f215d5992671b20dffed5747533b7
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-02-06 14:46:24 -0400

    Enhancements to the connection string for test and production

--- backend_node/src/helpers/redis.js
+++ backend_node/src/helpers/redis.js
@@ -1,11 +1,13 @@
 const { createNodeRedisClient } = require("handy-redis");
 
-const dbNumber = process.env.REDISDB;
+const db = process.env.REDISDB;
+const host = process.env.NODE_ENV === "testing" ? "127.0.0.1" : "redis";
+const port = 6379;
 
 const client = createNodeRedisClient({
-	host: "redis",
-	port: 6379,
-	db: dbNumber,
+	host,
+	port,
+	db,
 });
 
 module.exports = client;
