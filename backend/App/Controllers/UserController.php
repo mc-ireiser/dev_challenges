f62696d1d20f212f7a2a00afc11a5edf9b19be07
Jesus Rodriguez <mc.ireiser@gmail.com>
2021-02-06 20:18:42 -0400

    Clean

--- backend/App/Controllers/UserController.php
+++ backend/App/Controllers/UserController.php
@@ -6,7 +6,6 @@ namespace App\Controllers;
 
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
-use App\Controllers\Utils\ReturnResponse;
 use App\RedisConnector;
 use RedisException;
 
